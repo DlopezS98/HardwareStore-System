e0117c77351a3600caad106037606d84517a5e22
DlopezS98 <01dlopezs98@gmail.com>
2020-10-31 23:04:43 -0600

    Minor Changes on the proyects, String Connection to database

--- HardwareStore.Domain/DbConnection.cs
+++ HardwareStore.Domain/DbConnection.cs
@@ -11,19 +11,19 @@ namespace HardwareStore.Domain
 {
     public abstract class DbConnection
     {
-        //private readonly HardwareStoreEntities Context;
-        //public DbConnection()
-        //{
-        //    this.Context = new HardwareStoreEntities();
-        //}
+        private readonly HardwareStoreEntities Context;
+        public DbConnection()
+        {
+            this.Context = new HardwareStoreEntities();
+        }
 
         protected SqlConnection GetConnection()
         {
             //string stringConnection = @"" + ConfigurationManager.ConnectionStrings["HardwareStoreEntities"].ConnectionString;
             //ConnectionStringSettings Settings = ConfigurationManager.ConnectionStrings["HardwareStoreEntities"];
             //string stringConnection = @"" + Settings.ConnectionString;
-            //string stringConnection = @"" + this.Context.Database.Connection.ConnectionString;
-            string stringConnection = @"data source=01DLOPES98\DEVSTACK;initial catalog=HardwareStore;user id=DlopezS98;password=01DlopezS98;MultipleActiveResultSets=True;";
+            string stringConnection = @"" + this.Context.Database.Connection.ConnectionString;
+            //string stringConnection = @"data source=01DLOPES98\DEVSTACK;initial catalog=HardwareStore;user id=DlopezS98;password=01DlopezS98;MultipleActiveResultSets=True;";
             return new SqlConnection(stringConnection);
         }
     }
